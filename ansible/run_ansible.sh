3a2f48f8b5de015face20c69d28620d227418096
noortl1012 <nourtlili746@gmail.com>
2024-09-01 19:58:29 +0100

    first commit

---
+++ ansible/run_ansible.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/noor/.local/bin/ansible-playbook -i /mnt/c/Users/nourt/pytest-fastapi-crud-example/ansible/inventory.yml /mnt/c/Users/nourt/pytest-fastapi-crud-example/ansible/deploy.yml
